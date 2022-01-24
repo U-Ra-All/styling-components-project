ce2a2fcb7ea2a89fe81a7dbe8d333b3e5b60a83b
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-24 14:39:12 +0200

    02-Динамическое Добавление Стилей Inline

---
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./TaskInput.css";
+
+const TaskInput = (props) => {
+  const [inputText, setInputText] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
+
+  const taskInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsInputValid(true);
+    }
+    setInputText(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (inputText.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
+    props.onAddTask(inputText);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{ color: !isInputValid ? "red" : "black" }}>Задачи</label>
+        <input
+          style={{
+            borderColor: !isInputValid ? "red" : "black",
+            background: !isInputValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={taskInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Добавить Задачу</Button>
+    </form>
+  );
+};
+
+export default TaskInput;
