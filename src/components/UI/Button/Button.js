9a6ee5cae795b906d12fe15e13b8ba9ac87c7b46
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-01-27 10:40:36 +0200

    Styled Components и Media Queries

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 // tagged template literals
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   color: white;
@@ -9,6 +10,10 @@ const Button = styled.button`
   border-radius: 4px;
   box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
   cursor: pointer;
+
+  @media (min-width: 700px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
